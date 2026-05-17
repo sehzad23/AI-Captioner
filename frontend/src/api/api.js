b96f9fd3aaa9c8c7202d94f92030171eb4eb0979
sehzad23 <sehaans87@gmail.com>
2026-05-17 19:43:20 +0530

    fix: update API base URL to use environment variable and add redirects file

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: import.meta.env.BASE_URL,
   withCredentials: true, // 🔥 cookie ke liye
   // headers: {
   //   "Content-Type": "application/json",
