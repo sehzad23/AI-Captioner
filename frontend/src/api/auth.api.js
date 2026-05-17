af9dc82b8854f510d6b5f05d06516ef041f9dea3
sehzad23 <sehaans87@gmail.com>
2026-05-17 20:06:53 +0530

    fix: update API endpoints to include the correct prefix

--- frontend/src/api/auth.api.js
+++ frontend/src/api/auth.api.js
@@ -1,25 +1,25 @@
 import api from "./api";
 
 export const registerUser = async (data) => {
-  const res = await api.post("/auth/register", data);
+  const res = await api.post("/api/auth/register", data);
   return res.data;
 };
 
 export const loginUser = async (data) => {
-  const res = await api.post("/auth/login", data);
+  const res = await api.post("/api/auth/login", data);
   return res.data;
 };
 
 export const logoutUser = async () => {
-  return api("/auth/logout", {
+  return api("/api/auth/logout", {
     method: "GET",
   });
 };
 
 export const deleteUser = () => {
-  return api("/auth/delete", {
+  return api("/api/auth/delete", {
     method: "DELETE",
   });
 };
 
-export const getMe = () => api("/auth/me");
+export const getMe = () => api("/api/auth/me");
--- frontend/src/api/post.api.js
+++ frontend/src/api/post.api.js
@@ -1,6 +1,6 @@
 import api from "./api"
 
 export const createPost = async (formData) => {
-  const res = await api.post("/posts", formData);
+  const res = await api.post("/api/posts", formData);
   return res.data;
 };
